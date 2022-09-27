42cc5d664ff26d9057339608e8aeb78698ce41cb
anwarderbali <anwarderbali58@gmail.com>
2022-09-27 20:42:31 +0100

    cp-cloud

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import component from './component';
+import Component from './component';
 function App() {
   return (
     <div className="App">
-      <component/>
+      <Component/>
       
       
           </div>
